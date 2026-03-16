457be05b22233fab268f52196798cdda4c922d4a
Builder <builder@cursor.dev>
2026-03-16 23:08:19 +0000

    feat: add microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter, Nunito } from "next/font/google";
 import { SessionProvider } from "@/components/providers/SessionProvider";
 import { Nav } from "@/components/layout/Nav";
@@ -90,7 +91,18 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <head><MetaPixel /></head>
+      <head>
+        <MetaPixel />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vww12jf0yr");
+          `}
+        </Script>
+      </head>
       <body className={`${inter.variable} ${nunito.variable} font-sans antialiased min-h-screen flex flex-col`}>
         <PostHogProvider>
         <SessionProvider>
